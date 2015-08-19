31738e5726d1ac75359c00439ce29fa3dea8d4f4
John Austin <johnau@microsoft.com>
2015-08-19 15:44:49 -0700

    Initial commit

---
+++ C#/ReadMe.txt
@@ -0,0 +1,3 @@
+Instruction to developers:
+
+Open the *.sln file in Visual Studio. In the Properties pane of Visual Studio, change the Site URL value to absolute address of your development test site. For example, "http://MyDevServer/". Be sure to include the closing forward slash.
---
+++ C#/TooltipsWeb/Scripts/Tooltips.js
@@ -0,0 +1,31 @@
+﻿// This function is run when the app is ready to start 
+// interacting with the host application. It ensures 
+// the DOM is ready before running the rest of the code.
+Office.initialize = function (reason) {
+    $(document).ready(function () {
+        $('#span_help_01').hide();
+        $('#span_help_02').hide();
+        // Get the position of the keywords.
+        var tooltip01Position = $('#keyword01').offset();
+        var tooltip02Position = $('#keyword02').offset();
+        $('#keyword01').mouseover(function () {
+            // Show the tool tip at the specified position
+            // and then slowly fade away.
+           $('#span_help_01').show();
+           $('#span_help_01').css({
+            'position': 'absolute',
+            'left': tooltip01Position.left,
+            'top': tooltip01Position.top + 20
+           }).fadeOut(4500);
+        }); //end of keyword01 mouseover
+        $('#keyword02').mouseover(function() {
+            $('#span_help_02').show();
+            $('#span_help_02').css({
+                'position': 'absolute',
+                'left': tooltip02Position.left,
+                'top': tooltip02Position.top + 20
+            }).fadeOut(4500);
+        }); //end of keyword02 mouseover
+    });
+};
+
